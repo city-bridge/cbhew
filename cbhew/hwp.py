18580f9da9b5c3f3a62463f7bb7029b05455feba
city-bridge <hironori0130@gmail.com>
2024-10-20 17:36:49 +0900

    add hwp

---
+++ cbhew/hwp.py
@@ -0,0 +1,89 @@
+""".hwpファイルに関する処理
+"""
+import cbhew.config_file as config_file
+import cbhew.hwp_option as hwp_option
+
+class HwpParser:
+    
+    def load_hwp(self, hwp_text):
+        """hwpのテキストからパース
+        """
+        self.root = config_file.load(hwp_text)
+
+    def get_category_contents_table(self,category_name:str)->list:
+        """指定のカテゴリを2次元配列で取得
+        """
+        return self.root[category_name]
+
+    def get_category_contents_dict_list(self,category_name:str,key_list:list)->list:
+        """指定のカテゴリを2次元配列で取得し、列に名前を付ける
+        """
+        result = []        
+        table = self.get_category_contents_table(category_name)
+        for row in table:
+            col_dict = {}
+            for key in key_list:
+                col_dict[key] = None
+            count = 0
+            for col in row:
+                if count <= len(key_list):
+                    col_dict[key_list[count]] = col
+                    count = count + 1
+            result.append(col_dict)
+        return result
+
+    def get_database_version(self)->str:
+        """[DATABASE_VERSION]の内容取得
+        """
+        table = self.get_category_contents_table("DATABASE_VERSION")
+        return table[0][0]
+
+    def get_configurations(self)->list:
+        """[CONFIGURATIONS]の内容取得
+        """
+        key_list = ["name","path"]
+        configurations = self.get_category_contents_dict_list("CONFIGURATIONS", key_list)
+        return configurations
+
+    def get_options_xxx(self,conf_name:str, id_sort:bool=True)->list:
+        """[OPTIONS_xxx]の内容取得
+        """
+        key_list = ["data","id"]
+        category_name = "OPTIONS_" + conf_name
+        options_xxx = self.get_category_contents_dict_list(category_name,key_list)
+        if id_sort:
+            options_xxx = sorted(options_xxx, key=lambda x: x.get('id', 0))
+        return options_xxx
+
+    def get_options_xxx_ana(self,conf_name:str)->dict:
+        """[OPTIONS_xxx]の内容解析
+        """
+        result = {
+            "DEFINE":[],
+            "INCLUDE":[],
+            "PREINCLUDE":[]
+        }
+        options_xxx = self.get_options_xxx(conf_name)
+        for options_xxx_line in options_xxx:
+            try:
+                print("fdsafsda:",options_xxx_line["data"])
+                options = hwp_option.parse(options_xxx_line["data"])
+                print("options:",options)
+                for values in options:
+                    if values[0] == "S" and values[1] == "DEFINE":
+                        val = values[2:]
+                        result["DEFINE"].extend(val)
+                    elif values[0] == "S" and values[1] == "INCLUDE":
+                        val = values[2:]
+                        result["INCLUDE"].extend(val)
+                    elif values[0] == "S" and values[1] == "PREINCLUDE":
+                        val = values[2:]
+                        result["PREINCLUDE"].extend(val)
+
+            except Exception as e:
+                #print(e)
+                #raise e
+                #parser_optionsに形式が合わないものは無視
+                pass
+        return result
+    
---
+++ cbhew/hwp_option.py
@@ -0,0 +1,47 @@
+''' hwpファイル内の[OPTIONS_xxx]の1行分パースするためのモジュール
+'''
+from lark import Lark
+from lark import Transformer
+import pkgutil
+
+class HwpOptionTransformer(Transformer):
+    data:dict = {}
+    now_category:str = None
+    def options(self,tree):
+        print("trans options",tree)
+
+        return tree
+
+    def option(self,tree):
+        return tree
+    
+    def value(self,tree):
+        return tree[0]
+    
+    def SIGNED_NUMBER(self,tree):
+        return int(tree)
+    
+    def FLOAT(self,tree):
+        return float(tree)
+
+    def ESCAPED_STRING(self,tree):
+        return str(tree)
+    
+    def EMPTY_STRING(self,tree):
+        return ""
+    
+    def KEY_STRING(self, tree):
+        return str(tree)
+
+
+_parser = Lark(str(pkgutil.get_data('cbhew', 'hwp_option.lark'), encoding='utf-8'),
+               parser='lalr',
+               transformer=HwpOptionTransformer())
+
+
+def parse(option_data_text:str)->dict:
+    """fileを読み込んでparseしてdictに変換
+    """
+    return _parser.parse(option_data_text)
+
+
